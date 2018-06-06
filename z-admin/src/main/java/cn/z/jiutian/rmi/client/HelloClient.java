c2ef5ae184eee8ee47adb1d98a8e2602bfea0947
wanglongxu <wanglongxu@creditcrest.cn>
2018-06-06 18:00:34 +0800

    update

--- z-admin/src/main/java/cn/z/jiutian/rmi/client/HelloClient.java
+++ z-admin/src/main/java/cn/z/jiutian/rmi/client/HelloClient.java
@@ -14,7 +14,7 @@ public class HelloClient {
             Hello h = (Hello) Naming.lookup("rmi://192.168.5.71:12312/Hello");
             System.out.println(h.sayHello("zx"));
         } catch (MalformedURLException e) {
-            
+            System.out.println("url格式异常");
         }catch (RemoteException e) {
             System.out.println("创建对象异常");
             e.printStackTrace();
