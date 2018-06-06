9091c17660ce43e39bad74ce1e9ef2bc94c08c77
wanglongxu <wanglongxu@creditcrest.cn>
2018-06-06 14:47:12 +0800

    update jni

---
+++ z-admin/src/test/java/cn/z/jiutian/jni/HelloJni.java
@@ -0,0 +1,14 @@
+package cn.z.jiutian.jni;
+
+
+public class HelloJni {
+
+    static{
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+    
+    public static void main(String[] args) {
+        new HelloJni();
+    }
+}
