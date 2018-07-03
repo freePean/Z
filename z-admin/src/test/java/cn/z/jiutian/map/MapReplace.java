2bea5cbc65b05c25baea8f57773ea225b9564e81
wanglongxu <wanglongxu@creditcrest.cn>
2018-07-03 11:49:58 +0800

    update

--- z-admin/src/test/java/cn/z/jiutian/map/MapReplace.java
+++ z-admin/src/test/java/cn/z/jiutian/map/MapReplace.java
@@ -1,23 +1,54 @@
 package cn.z.jiutian.map;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 public class MapReplace {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MapReplace.class);
+    
     @Test
     public void replaceTest(){
         Map<String,String> maps = new HashMap<String,String>();
-        maps.put("1", "2");
+        maps.put("1", "Doug");
+        maps.put("2", "joy");
+        maps.put("3", "linda");
         
-        maps.replace("1", "2", "new value");
+        /*
+         * 1
+         */
+        for(Map.Entry<String, String> entry : maps.entrySet()){
+            LOG.info("maps 1 遍历    key : " + entry.getKey() + ", value : " + entry.getValue());
+        }
         
-        for(Entry entry : maps.entrySet()){
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
+        /*
+         * 2
+         */
+        for(String str : maps.keySet()){
+            LOG.info("maps 2 遍历   key : " + str + ", value : " + maps.get(str));
+        }
+        
+        /*
+         * 3
+         */
+        Iterator<Map.Entry<String, String>> iterators = maps.entrySet().iterator();
+        
+        while(iterators.hasNext()){
+            Map.Entry<String, String> entry = iterators.next();
+            
+            LOG.info("maps 3 遍历    key : " + entry.getKey() + ", value : " + entry.getValue());
+        }
+        
+        /**
+         * 4
+         */
+        for(String str : maps.values()){
+            LOG.info("maps 4 遍历   value : " + str);
         }
     }
 
