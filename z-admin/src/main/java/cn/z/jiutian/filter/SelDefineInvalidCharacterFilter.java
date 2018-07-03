364d40aaec6a6b7602628ab3ee970a8ba14d38c3
wanglongxu <wanglongxu@creditcrest.cn>
2018-07-03 11:33:52 +0800

    update

--- z-admin/src/main/java/cn/z/jiutian/filter/SelDefineInvalidCharacterFilter.java
+++ z-admin/src/main/java/cn/z/jiutian/filter/SelDefineInvalidCharacterFilter.java
@@ -2,6 +2,7 @@ package cn.z.jiutian.filter;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,6 +13,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.Multiset.Entry;
 
 public class SelDefineInvalidCharacterFilter implements Filter{
 
@@ -30,22 +33,33 @@ public class SelDefineInvalidCharacterFilter implements Filter{
         
         @SuppressWarnings("unchecked")
         Enumeration<String> allParameter = request.getAttributeNames();
+        Map<String, String> map = request.getParameterMap();
         
-        while(allParameter.hasMoreElements()){
-            parameterName = allParameter.nextElement();
-            parameterValue = request.getParameter(parameterName);
-            
-            if(null != parameterName){
-                for (String str : invalidCharacter) {
-                    if(StringUtils.containsIgnoreCase(str, parameterValue)){
-                        request.setAttribute("errorMessage", "非法字符： " + str);
-                        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/error.jsp");
-                        requestDispatcher.forward(request, response);
-                        return;
-                    }
+        for(String key : map.keySet()){
+            for (String str : invalidCharacter) {
+                if(StringUtils.containsIgnoreCase(key, str)){
+                    request.setAttribute("errorMessage", "非法字符 : " + str);
+                    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/error.jsp");
+                    requestDispatcher.forward(request, response);
+                    return ;
                 }
             }
         }
+//        while(allParameter.hasMoreElements()){
+//            parameterName = allParameter.nextElement();
+//            parameterValue = request.getParameter(parameterName);
+//            
+//            if(null != parameterName){
+//                for (String str : invalidCharacter) {
+//                    if(StringUtils.containsIgnoreCase(str, parameterValue)){
+//                        request.setAttribute("errorMessage", "非法字符： " + str);
+//                        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/error.jsp");
+//                        requestDispatcher.forward(request, response);
+//                        return;
+//                    }
+//                }
+//            }
+//        }
         chain.doFilter(request, response);
     }
     
