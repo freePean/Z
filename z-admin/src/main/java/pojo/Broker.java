afa02bbe553a93c029d51c77e2dd90b0953fb7f3
wanglx077 <358687516@qq.com>
2021-03-09 13:24:21 +0800

    11

--- z-admin/src/main/java/pojo/Broker.java
+++ z-admin/src/main/java/pojo/Broker.java
@@ -1,22 +1,42 @@
 package pojo;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class Broker {
 
-	@Before("execution(* pojo.Landlord.service())")
-	public void before() {
+//	@Pointcut("execution(* pojo.Landlord.service())")
+//	public void IService() {
+//		
+//	}
+//	
+//	@Before("IService()")
+//	public void before() {
+//		System.out.println("带租客看房");
+//		System.out.println("谈价格");
+//	}
+//	
+//	@After("IService()")
+//	public void after() {
+//		System.out.println("交钥匙");
+//	}
+	
+	@Around("execution(* pojo.Landlord.service())")
+	public void around(ProceedingJoinPoint joinPoint) {
 		System.out.println("带租客看房");
 		System.out.println("谈价格");
-	}
-	
-	@After("execution(* pojo.Landlord.service())")
-	public void after() {
+		
+		try {
+			joinPoint.proceed();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		System.out.println("交钥匙");
 	}
 	
